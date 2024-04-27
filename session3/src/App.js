b9dfca1249b34b0d8a7c6ebe9321849094ccedac
kimseohyun <lsabella0104@naver.com>
2024-04-27 15:57:55 +0900

    수정했습니다

--- session3/src/App.js
+++ session3/src/App.js
@@ -15,42 +15,12 @@ function App() {
         </div>
     </>
         <div id="keyword">
-            <h3>나를 표현하는 4가지 키워드</h3>
-            <ul>
-                <li>성실함</li>
-                <li>진솔함</li>
-                <li>생각</li>
-                <li>창의력</li>
-            </ul>
         </div>
         <div id="value">
             <h3>가족 구성원</h3>
-            <ol>
-                <li>어머니</li>
-                <li>아버지</li>
-                <li>파이(강아지)</li>
-            </ol>
         </div>
         <div id="favorite">
             <h3>내가 좋아하는 것</h3>
-            <table>
-                <thead>
-                    <tr>
-                        <th>음악</th>
-                        <th>과일</th>
-                        <th>취미</th>
-                        <th>동물</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>밴드 음악</td>
-                        <td>딸기</td>
-                        <td>베이스 연주</td>
-                        <td>강아지</td>
-                    </tr>
-                </tbody>
-            </table>
         </div>
 
         <div id="MyLink">
--- session3/src/TopWrapper.js
+++ session3/src/TopWrapper.js
@@ -14,6 +14,42 @@ const TopWrapper = () => {
             <span>60211900</span>
             <span>김서현</span>
         </p>
+        <p id="keyword">
+        <h3>나를 표현하는 4가지 키워드</h3>
+            <ul>
+                <li>성실함</li>
+                <li>진솔함</li>
+                <li>생각</li>
+                <li>창의력</li>
+            </ul>
+        </p>
+        <p id="value">
+        <ol>
+                <li>어머니</li>
+                <li>아버지</li>
+                <li>파이(강아지)</li>
+            </ol>
+        </p>
+        <p id="favorite">
+        <table>
+                <thead>
+                    <tr>
+                        <th>음악</th>
+                        <th>과일</th>
+                        <th>취미</th>
+                        <th>동물</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>밴드 음악</td>
+                        <td>딸기</td>
+                        <td>베이스 연주</td>
+                        <td>강아지</td>
+                    </tr>
+                </tbody>
+            </table>
+        </p>
       </div>
     </div>
   );
